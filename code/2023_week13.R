d12055bc13e8714c3cc9678472f9c929be472e51
JasonBernstein1 <117031728+JasonBernstein1@users.noreply.github.com>
2023-04-10 07:46:47 -0600

    Improve tibble names

--- code/2023_week13.R
+++ code/2023_week13.R
@@ -13,7 +13,7 @@ min_date <- as.Date("1960-01-01", format = "%Y-%m-%d")
 max_date <- as.Date("2023-04-09", format = "%Y-%m-%d") 
 
 # get most recent DST status for each location in data
-df_dst <- dfs$transitions |>
+dst_status <- dfs$transitions |>
   mutate(begin = lubridate::as_datetime(begin)) |>
   filter(begin > min_date & begin < max_date) |>
   select(zone, begin, dst) |>
@@ -24,30 +24,30 @@ df_dst <- dfs$transitions |>
   mutate(dst_active = ifelse(dst, 'DST Is Active', 'DST Is Inactive'))
 
 # Phoenix is currently not in DST
-df_dst |> filter(str_detect(zone, 'Phoenix')) |> select(zone, dst)
+dst_status |> filter(str_detect(zone, 'Phoenix')) |> select(zone, dst)
 
 # Indiana has 11 locations currently in DST (3 missing coordinates)
-df_dst |> filter(str_detect(zone, 'Indiana')) |> select(zone, dst)
+dst_status |> filter(str_detect(zone, 'Indiana')) |> select(zone, dst)
 
 # coordinate reference system (CRS) for World Geodetic System 1984
 crs_wgs84 <- 4326
 
 # spatial data frame of country polygons
-spdf_countries <- rnaturalearth::ne_countries(returnclass = "sf") |>
+countries <- rnaturalearth::ne_countries(returnclass = "sf") |>
   st_transform(crs = crs_wgs84)
 
-# convert data to sf class
-spdf_dst <- df_dst |>
+# convert data to sf class so can plot with geom_sf
+dst_status_sf <- dst_status |>
   # remove points with missing coordinates
   filter(!is.na(latitude)) |>
   st_as_sf(coords = c("longitude", "latitude"), crs = crs_wgs84)
 
 # create plot indicating locations where DST is active or not
-spdf_countries |>
+countries |>
   ggplot() + 
   geom_sf(fill = 'white') +
-  geom_sf(data = spdf_dst, aes(fill = dst_active), alpha = 0.7, size = 2,
-          shape = 21) +
+  geom_sf(data = dst_status_sf, aes(fill = dst_active), alpha = 0.7,
+          size = 2, shape = 21) +
   coord_sf(expand = F) +
   labs(x = 'Longitude', y = 'Latitude', fill = '',
        title = 'Where is Daylight Savings Time (DST) Currently Active?',
