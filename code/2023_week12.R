75b59da98c6ee812f058d31d7f893231793912f3
JasonBernstein1 <117031728+JasonBernstein1@users.noreply.github.com>
2023-04-15 16:37:57 -0600

    Initial commit for 2023, week 12

---
+++ code/2023_week12.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(tidytuesdayR)
+
+# This script plots the 53 programming languages with the largest
+# number of jobs per user. R is the 53rd language on this list.
+
+tuesdata <- tidytuesdayR::tt_load(2023, week = 12)
+
+# The type variable in tuesdata$languages is written in lower camel case.
+# This function converts lower camel case to sentence case.
+camelcase_to_sentence = function(s) {
+  str_replace_all(string = s, pattern = '([A-Z])', replacement = ' \\1') |>
+    str_to_lower() |>
+    str_to_sentence()
+}
+
+# test can convert strings from camel case to sentence case
+stopifnot(
+  camelcase_to_sentence(s = 'abcDefGhi') == "Abc def ghi",
+  camelcase_to_sentence(s = 'dataNotation') == "Data notation"
+)
+
+# format data for plot
+df <- tuesdata$languages |>
+  rename_with(~ str_replace(.x, "number_of", "n")) |>
+  # remove languages with no users since jobs_per_user is then undefined
+  filter(n_users > 0) |>
+  mutate(jobs_per_user = n_jobs / n_users) |>
+  mutate(rank = rank(-jobs_per_user)) |>
+  # expand pl, idl acronyms
+  mutate(type = case_when(
+    type == 'idl' ~ 'interfaceDefinitionLanguage',
+    type == 'pl' ~ 'programmingLanguage',
+    TRUE ~ type
+  )) |>
+  # improve formatting of type variable for clearer plot legend
+  mutate(type = camelcase_to_sentence(type)) |>
+  mutate(type = case_when(
+    type == 'Xml format' ~ 'XML format',
+    TRUE ~ type
+  ))
+
+# COBOL ranks 50th, R ranks 53rd, SAS ranks 55th in jobs per user
+df |>
+  filter(title %in% c('COBOL', 'R', 'SAS')) |>
+  select(title, rank, n_jobs, n_users, jobs_per_user) |>
+  arrange(rank)
+
+# plot programming languages by number of jobs per user
+df |>
+  slice_max(jobs_per_user, n = 53) |>
+  ggplot(aes(x = reorder(title, jobs_per_user), y = jobs_per_user)) +
+  geom_col(aes(fill = type), width = 0.75) +
+  geom_label(aes(label = title, fill = type), size = 2.5,
+             label.size = 0.05, show.legend = FALSE) +
+  scale_y_log10(limits = c(0.01, 100)) +
+  coord_flip() +
+  labs(
+    x = '',
+    y = 'Jobs per User',
+    fill = 'Language Type',
+    title = 'Fifty-three Top Languages by Jobs per User',
+    caption = 'TidyTuesday: 2023, week 12.'
+  ) +
+  theme_bw() +
+  theme(
+    legend.position = c(.76, .5),
+    legend.text = element_text(size = 12),
+    axis.text = element_text(size = 10),
+    plot.title = element_text(size = 18),
+    panel.grid.minor = element_line(color = NA),
+    panel.grid.major.y = element_line(color = NA)
+  )
+
+ggsave(filename = './images/2023_week12.png', bg = 'white')
