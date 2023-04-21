4731fc10e4a19120b42c55cb4d7661be1aa9126b
JasonBernstein1 <117031728+JasonBernstein1@users.noreply.github.com>
2023-04-20 19:50:16 -0600

    Initial commit for 2023, week 16

---
+++ code/2023_week16.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(tidytuesdayR)
+library(ggmap)
+library(glue)
+
+# This script plots locations of Neolithic founder crops on a map
+# of the region.
+
+tuesdata <- tidytuesdayR::tt_load(2023, week = 16)
+
+df <- tuesdata$founder_crops |>
+  filter(!is.na(category)) |>
+  select(latitude, longitude, category) |>
+  # create variable of category counts to show in facet label
+  add_count(category) |>
+  mutate(facet_label = glue('{category} (n = {n})'))
+
+# map boundaries
+bound_box <- with(df,
+                  c(0.99 * min(longitude), 0.99 * min(latitude),
+                    1.01 * max(longitude), 1.01 * max(latitude)))
+
+base_map <- ggmap(get_stamenmap(bbox = bound_box, zoom = 7,
+                                maptype = "terrain"))
+
+# create separate map for each crop category
+base_map +
+  geom_point(data = df,
+           aes(x = longitude, y = latitude, fill = category), 
+           shape = 21, alpha = 0.7, size = 3) +
+  facet_wrap(~ facet_label, strip.position = "bottom") +
+  labs(
+    x = '',
+    y = '',
+    title = 'Locations of Neolithic Founder Crops',
+    caption = 'TidyTuesday: 2023, week 16.'
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text = element_blank(),
+    strip.text.x = element_text(size = 12),
+    legend.position = 'none',
+    plot.title = element_text(hjust = 0.5, size = 18)
+  )
+
+ggsave(filename = './images/2023_week16.png', bg = 'white',
+       height = 6, width = 9)
