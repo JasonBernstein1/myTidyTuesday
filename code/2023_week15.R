e958759d9dcec5654dad9a3fd0cefacd9d1b4f9f
JasonBernstein1 <jstatbernstein@gmail.com>
2023-06-07 21:26:44 -0600

    Redo plot, improve colors, remove facets

--- code/2023_week15.R
+++ code/2023_week15.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(tidytuesdayR)
 
-# This script plots the egg-to-hen ratio by production process and 
+# This script plots the egg-to-hen ratio by production process and
 # whether the eggs are for hatching or the table.
 
 tuesdata <- tidytuesdayR::tt_load(2023, week = 15)
@@ -14,8 +14,8 @@ eggproduction <- tuesdata$`egg-production` |>
 with(eggproduction, table(prod_process, prod_type))
 
 # The egg-to-hen ratio for 'cage-free (organic)' and 'cage-free (non-organic)'
-# production processes are similar. Here we check ratios are similar for 
-# organic and non-organic production. Hence, we lump all cage-free eggs 
+# production processes are similar. Here we check ratios are similar for
+# organic and non-organic production. Hence, we lump all cage-free eggs
 # together for the final plot.
 eggproduction |>
   filter(str_starts(prod_process, 'cage-free')) |>
@@ -40,40 +40,53 @@ df <- eggproduction |>
   mutate(prod_type = str_to_sentence(prod_type)) |>
   mutate(prod_process = str_to_sentence(prod_process)) |>
   # remove ' (organic)' from 'Cage-free (organic)'
-  mutate(prod_process = str_remove(prod_process, ' \\(organic\\)'))
+  mutate(prod_process = str_remove(prod_process, ' \\(organic\\)')) |>
+  # combine production type and process for plotting
+  mutate(prod_method = glue::glue("{prod_type} ({prod_process})") |>
+         fct_reorder(.x = egg_to_hen_ratio, .fun = mean, .desc = TRUE))
 
 # plot the egg-to-hen ratio over time
 df |>
   ggplot() +
-  geom_line(aes(x = observed_month, y = egg_to_hen_ratio, col = prod_process,
-                group = prod_process), linewidth = 1.1) +
+  geom_line(aes(x = observed_month, y = egg_to_hen_ratio,
+                col = prod_method), linewidth = 1.5) +
   # add vertical lines at Feb. 28 since noticeable dips here
-  geom_vline(xintercept = as.Date(paste(2017:2022, "-02-28", sep = "")),
+  geom_vline(xintercept = as.Date(paste0(2017:2021, "-02-28")),
              linetype = 'dashed', alpha = 0.6) +
+  scale_x_continuous(
+    breaks = as.Date(paste0(2017:2021, "-02-28")),
+    labels = paste('Feb. 28,\n', 2017:2021)
+  ) +
+  scale_y_continuous(
+    limits = c(16, 26),
+    breaks = seq(16, 26, by = 2)
+  ) +
+  scale_color_manual(
+    values = thematic::okabe_ito(3)
+  )+
   labs(
-    x = 'Date',
-    y = 'Egg-to-Hen Ratio',
-    col = 'Production Process',
-    title = 'Egg-to-Hen Ratio',
-    subtitle = 'July 2016 - Feb. 2021',
-    caption = 'Vertical lines indicate Feb. 28.\n
-               TidyTuesday: 2023, week 15.'
+    x = element_blank(),
+    y = 'Egg-to-Hen Ratio\n',
+    col = 'Egg Production Type',
+    title = 'Monthly Egg-to-Hen Ratio from July 2016 to Feb. 2021',
+    caption = 'TidyTuesday: 2023, week 15.'
   ) +
-  scale_color_manual(values = c("darkgreen", "red")) +
-  theme_bw(base_size = 12) +
   theme(
-    axis.text = element_text(size = 12),
-    strip.text.x = element_text(size = 12),
-    legend.text = element_text(size = 12),
-    legend.position = 'top',
-    plot.title = element_text(hjust = 0.5, size = 18),
-    plot.subtitle = element_text(hjust = 0.5, vjust = -0.7, size = 14),
-    strip.background = element_rect(fill = 'lightgray', color = 'black'),
-    panel.background = element_rect(fill = 'beige', color = 'black'),
-    panel.grid.major = element_line(color = 'gray', linetype = 'dotted'),
-    panel.grid.minor = element_blank()
-  ) +
-  facet_grid(~ prod_type)
+    axis.text = element_text(size = 14),
+    axis.title = element_text(size = 18),
+    legend.background = element_rect(fill = '#FFFAFA', color = 'black'),
+    legend.key = element_rect(fill = '#FFFAFA'),
+    legend.position = c(0.855, 0.125),
+    legend.text = element_text(size = 14),
+    legend.title = element_text(size = 14),
+    panel.background = element_rect(fill = '#FFFAFA'),
+    panel.border = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(color = 'gray', linetype = 'dotted'),
+    panel.grid.minor = element_blank(),
+    plot.background = element_rect(fill = '#FFFAFA'),
+    plot.title = element_text(size = 26),
+  )
 
-ggsave(filename = './images/2023_week15.png', bg = 'beige',
+ggsave(filename = './images/2023_week15.png', bg = '#FFFAFA',
        height = 10, width = 15)
