3727ff10dd7c633bb2c9d9509b4a0892d5039342
JasonBernstein1 <jstatbernstein@gmail.com>
2023-08-13 21:34:44 -0600

    Update script following Tidyverse style guide

--- code/2023_week16.R
+++ code/2023_week16.R
@@ -12,39 +12,50 @@ df <- tuesdata$founder_crops |>
   select(latitude, longitude, category) |>
   # create variable of category counts to show in facet label,
   # and order subplots by increasing n_category
-  add_count(category, name = 'n_category') |>
-  mutate(facet_label = glue::glue('{category} (n = {n_category})') |>
-                       fct_reorder(.x = n_category))
+  add_count(category, name = "n_category") |>
+  mutate(
+    facet_label = glue::glue("{category} (n = {n_category})") |>
+      fct_reorder(.x = n_category)
+  )
 
-# map boundaries
-bound_box <- with(df,
-                  c(0.99 * min(longitude), 0.99 * min(latitude),
-                    1.01 * max(longitude), 1.01 * max(latitude)))
+map_boundaries <- with(
+  df,
+  c(
+    0.99 * min(longitude), 0.99 * min(latitude),
+    1.01 * max(longitude), 1.01 * max(latitude)
+  )
+)
 
-base_map <- ggmap(get_stamenmap(bbox = bound_box, zoom = 7,
-                                maptype = "terrain"))
+base_map <- ggmap::ggmap(get_stamenmap(
+  bbox = map_boundaries, zoom = 7,
+  maptype = "terrain"
+))
 
 # create separate map for each crop category
 base_map +
-  geom_point(data = df,
-           aes(x = longitude, y = latitude, fill = category),
-           shape = 21, alpha = 0.7, size = 3) +
+  geom_point(
+    data = df,
+    aes(x = longitude, y = latitude, fill = category),
+    shape = 21, alpha = 0.7, size = 3
+  ) +
   scale_fill_brewer(palette = "Dark2") +
-  facet_wrap(~ facet_label, strip.position = "bottom") +
+  facet_wrap(~facet_label, strip.position = "bottom") +
   labs(
     x = element_blank(),
     y = element_blank(),
-    title = 'Locations of Neolithic Founder Crops',
-    caption = 'TidyTuesday: 2023, week 16.'
+    title = "Locations of Neolithic Founder Crops",
+    caption = "TidyTuesday: 2023, week 16."
   ) +
   theme_minimal() +
   theme(
     axis.text = element_blank(),
     strip.text.x = element_text(size = 14),
-    legend.position = 'none',
-    plot.background = element_rect(color = 'white', fill = 'white'),
+    legend.position = "none",
+    plot.background = element_rect(color = "white", fill = "white"),
     plot.title = element_text(hjust = 0.5, size = 20)
   )
 
-ggsave(filename = './images/2023_week16.png',
-       height = 6, width = 8)
+ggsave(
+  filename = "./images/2023_week16.png",
+  height = 6, width = 8
+)
