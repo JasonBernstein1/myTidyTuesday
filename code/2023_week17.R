d4470117e661c2b228438febee0656e33b2e042c
JasonBernstein1 <jstatbernstein@gmail.com>
2023-08-15 06:24:17 -0600

    Update script following Tidyverse style guide

--- code/2023_week17.R
+++ code/2023_week17.R
@@ -9,53 +9,58 @@ tuesdata <- tidytuesdayR::tt_load(2023, week = 17)
 winners <- tuesdata$winners
 london_marathon <- tuesdata$london_marathon
 
-# wrangle data for plot
 df <- winners |>
-  # create variable indicating if record is men/women
-  mutate(sex = str_detect(Category, 'Women') |>
-               ifelse('Women', 'Men')) |>
-  # create variable indicating if record is wheelchair/running
-  mutate(race = str_detect(Category, 'Wheelchair') |>
-                ifelse('Wheelchair', 'Running')) |>
+  mutate(
+    sex = ifelse(str_detect(Category, "Women"), "Women", "Men"),
+    race = ifelse(str_detect(Category, "Wheelchair"), "Wheelchair", "Running")
+  ) |>
   # use 'Date' in london_marathon instead of 'Year' in winners
   left_join(london_marathon) |>
   select(Date, Time, sex, race)
 
-# plot time series of winning times by wheelchair/running and men/women
 df |>
   ggplot(aes(x = Date, y = Time, color = sex)) +
   geom_line(linewidth = 0.4) +
   geom_point(aes(shape = sex)) +
-  facet_wrap(~ race) +
-  scale_color_manual(values = c("Men" = "#FF8C00", "Women" = "#FFFACD"),
-                     name = NULL) +
-  scale_shape_manual(values = c("Men" = 1, "Women" = 2), name = NULL) +
-  scale_y_time(breaks = hms(c('02:00:00', '03:00:00', '04:00:00')),
-               labels = paste(2:4, 'hours')) +
+  facet_wrap(~race) +
+  scale_color_manual(
+    values = c("Men" = "#FF8C00", "Women" = "#FFFACD"),
+    name = NULL
+  ) +
+  scale_shape_manual(
+    values = c("Men" = 1, "Women" = 2),
+    name = NULL
+  ) +
+  scale_y_time(
+    breaks = hms(c("02:00:00", "03:00:00", "04:00:00")),
+    labels = paste(2:4, "hours")
+  ) +
   labs(
     x = element_blank(),
     y = element_blank(),
-    title = 'London Marathon Winning Times',
+    title = "London Marathon Winning Times",
     subtitle = element_blank(),
-    caption = 'TidyTuesday: 2023, week 17.',
+    caption = "TidyTuesday: 2023, week 17.",
     color = element_blank()
   ) +
   theme(
-    axis.text = element_text(color = 'white', size = 12),
-    legend.background = element_rect(fill = 'black'),
+    axis.text = element_text(color = "white", size = 12),
+    legend.background = element_rect(fill = "black"),
     legend.key = element_blank(),
     legend.position = c(0.85, 0.675),
-    legend.text = element_text(color = 'white', size = 16),
-    panel.background = element_rect(fill = 'black'),
+    legend.text = element_text(color = "white", size = 16),
+    panel.background = element_rect(fill = "black"),
     panel.grid.minor = element_blank(),
     panel.grid.major.x = element_blank(),
-    panel.grid.major.y = element_line(linetype = 'dashed', color = 'gray'),
-    plot.background = element_rect(fill = 'black', color = 'black'),
+    panel.grid.major.y = element_line(linetype = "dashed", color = "gray"),
+    plot.background = element_rect(fill = "black", color = "black"),
     plot.title = element_text(hjust = 0.5, size = 18),
-    strip.background = element_rect(fill = 'black'),
-    strip.text = element_text(color = 'white', size = 16),
-    text = element_text(color = 'white')
+    strip.background = element_rect(fill = "black"),
+    strip.text = element_text(color = "white", size = 16),
+    text = element_text(color = "white")
   )
 
-ggsave(filename = './images/2023_week17.png',
-  height = 6, width = 9)
+ggsave(
+  filename = "./images/2023_week17.png",
+  height = 6, width = 9
+)
--- code/2023_week18.R
+++ code/2023_week18.R
@@ -1,60 +1,63 @@
 library(tidyverse)
 library(tidytuesdayR)
 
-# This script plots the number of mice and rats observed over time
-# by treatment.
+# This script plots the number of mice and rats over time by treatment.
 
 tuesdata <- tidytuesdayR::tt_load(2023, week = 18)
 
 species <- tuesdata$species
 surveys <- tuesdata$surveys
 
-# wrangle data for plot
 df <- surveys |>
   left_join(species) |>
-  # determine if each record is of a mouse or a rat
-  mutate(mouse_or_rat = str_detect(commonname, 'mouse') |>
-                        ifelse('mouse', 'rat')) |>
+  mutate(
+    mouse_or_rat = ifelse(str_detect(commonname, "mouse"), "mouse", "rat")
+  ) |>
   # count records of mice and rats by treatment and year
-  count(year, treatment, mouse_or_rat, name = 'n') |>
+  count(year, treatment, mouse_or_rat, name = "n") |>
   # set n = 0 for missing year/treatment combinations
   complete(year, treatment, mouse_or_rat, fill = list(n = 0))
 
-# plot the number of mice and rats over time by treatment
 df |>
   ggplot() +
-  geom_line(aes(x = year, y = n, col = str_to_sentence(treatment)),
-            linewidth = 1.25) +
-  facet_wrap(~ mouse_or_rat,
-             strip.position = "left",
-             labeller = as_labeller(c(mouse = "Mice", rat = "Rats")),
-             ncol = 1) +
+  geom_line(
+    aes(x = year, y = n, col = str_to_sentence(treatment)),
+    linewidth = 1.25
+  ) +
+  facet_wrap(
+    ~mouse_or_rat,
+    strip.position = "left",
+    labeller = as_labeller(c(mouse = "Mice", rat = "Rats")),
+    ncol = 1
+  ) +
   scale_y_log10() +
   labs(
     x = element_blank(),
     y = element_blank(),
     col = element_blank(),
-    title = 'Rodent Counts by Treatment over Time',
-    caption = 'TidyTuesday: 2023, week 18.'
+    title = "Rodent Counts by Treatment over Time",
+    caption = "TidyTuesday: 2023, week 18."
   ) +
   theme(
-    axis.text = element_text(color = 'white', size = 12),
-    legend.background = element_rect(fill = 'black'),
+    axis.text = element_text(color = "white", size = 12),
+    legend.background = element_rect(fill = "black"),
     legend.key = element_blank(),
-    legend.position = 'top',
-    legend.text = element_text(color = 'white', size = 12),
-    panel.background = element_rect(fill = 'black'),
+    legend.position = "top",
+    legend.text = element_text(color = "white", size = 12),
+    panel.background = element_rect(fill = "black"),
     panel.grid.major.x = element_blank(),
-    panel.grid.major.y = element_line(linetype = 'dotted', color = 'gray'),
+    panel.grid.major.y = element_line(linetype = "dotted", color = "gray"),
     panel.grid.minor = element_blank(),
-    plot.background = element_rect(fill = 'black', color = 'black'),
-    plot.caption = element_text(color = 'white'),
+    plot.background = element_rect(fill = "black", color = "black"),
+    plot.caption = element_text(color = "white"),
     plot.title = element_text(hjust = 0.5, size = 18),
-    strip.background = element_rect(fill = 'black'),
+    strip.background = element_rect(fill = "black"),
     strip.placement = "outside",
-    strip.text = element_text(color = 'white', size = 16),
-    text = element_text(color = 'white')
+    strip.text = element_text(color = "white", size = 16),
+    text = element_text(color = "white")
   )
 
-ggsave(filename = './images/2023_week18.png',
-       height = 6, width = 9)
+ggsave(
+  filename = "./images/2023_week18.png",
+  height = 6, width = 9
+)
