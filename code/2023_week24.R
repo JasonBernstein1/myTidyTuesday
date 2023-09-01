eab2900a491e31cb6cec75dbb72ea6e3c5a2790c
JasonBernstein1 <jstatbernstein@gmail.com>
2023-09-01 14:31:06 -0600

    Improve formatting using styler

--- code/2023_week24.R
+++ code/2023_week24.R
@@ -11,18 +11,18 @@ tuesdata <- tidytuesdayR::tt_load(2023, week = 24)
 # wrangle data for table
 df <- tuesdata$safi_data |>
   select(village, items_owned) |>
-  filter(items_owned != 'NULL') |>
+  filter(items_owned != "NULL") |>
   tidytext::unnest_tokens(Item, items_owned) |>
   count(Item, village) |>
   pivot_wider(names_from = village, values_from = n, values_fill = 0) |>
   mutate(
     Item = Item |>
       str_to_sentence() |>
-      str_replace('_', ' '),
+      str_replace("_", " "),
     Item = case_when(
-        Item == 'Sterio' ~ 'Stereo',
-        TRUE ~ Item
-      ),
+      Item == "Sterio" ~ "Stereo",
+      TRUE ~ Item
+    ),
     Total = rowSums(across(where(is.numeric)))
   ) |>
   arrange(desc(Total))
@@ -32,7 +32,7 @@ start_date <- min(tuesdata$safi_data$interview_date)
 end_date <- max(tuesdata$safi_data$interview_date)
 
 # colors for the three different villages
-colors <- hcl.colors(n = 3, palette = 'Dark 2')
+colors <- hcl.colors(n = 3, palette = "Dark 2")
 
 # create table
 item_counts <- df |>
@@ -85,8 +85,8 @@ item_counts <- df |>
     ),
     locations = cells_source_notes()
   ) |>
-  tab_source_note('TidyTuesday: 2023, week 24 | Source: SAFI Survey')
+  tab_source_note("TidyTuesday: 2023, week 24 | Source: SAFI Survey")
 
 item_counts
 
-gtsave(item_counts, filename = 'images/2023_week24.png')
+gtsave(item_counts, filename = "images/2023_week24.png")
